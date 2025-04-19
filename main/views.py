d3033ab1a15d34eeb704e0945796d2d5a09599ae
Nazar Rachkivskiy <rachkovskiy.ua@gmail.com>
2025-04-19 17:31:31 +0300

    add views for comment, add comment to post

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CommentForm
 from .models import Post
 
 
@@ -18,5 +19,17 @@ def index(request):
 
 
 def post_detail(request, post_slug):
-    post = Post.objects.get(slug=post_slug)
-    return render(request, 'main/detail.html', {"post": post})
+    post = get_object_or_404(Post, slug=post_slug)
+    comments = post.comments.all()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.author = request.user
+            new_comment.save()
+            return redirect('main:post_detail', post_slug=post_slug)
+    else:
+        form = CommentForm()
+    return render(request, 'main/detail.html', {"post": post, 'comments': comments, "form": form})
